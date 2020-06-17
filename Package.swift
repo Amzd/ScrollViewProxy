c0be050ca45f52df649fe7b9255703ca56e29d5e
Casper Zandbergen <casper@yaacomm.com>
2020-06-17 17:31:44 +0200

    Added platform restriction

--- Package.swift
+++ Package.swift
@@ -5,11 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "ScrollViewProxy",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v11),
+        .tvOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ScrollViewProxy",
-            targets: ["ScrollViewProxy"]),
+            targets: ["ScrollViewProxy"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
