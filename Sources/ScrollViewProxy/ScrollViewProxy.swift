0a7e15a960a07eb7e35b967129926c988c57e217
AJ Caldwell <tomatrow@gmail.com>
2020-07-28 19:14:50 -0700

    Add available attribute to BindingWrapper

--- Sources/ScrollViewProxy/ScrollViewProxy.swift
+++ Sources/ScrollViewProxy/ScrollViewProxy.swift
@@ -33,6 +33,7 @@ extension View {
     }
 }
 
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 fileprivate enum BindingWrapper<Value> {
     case `internal`(State<Value>)
     case external(Binding<Value>)
