7dc575a663869b9296ef39ddbab81fc8c39f48d8
AJ Caldwell <tomatrow@gmail.com>
2020-10-10 16:07:37 -0700

    Change the package name to prevent conflicts

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "ScrollViewProxy",
+            name: "FauxScrollViewProxy",
             targets: ["ScrollViewProxy"]
         ),
     ],
